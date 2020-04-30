3aee59707f638db001b6c3e4493c20accd624484
Michael Sotir <sotirm@umich.edu>
2020-04-30 16:17:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  final_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
+  final_hash = {:railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25, }, :names => {}}}
   
 
 end
